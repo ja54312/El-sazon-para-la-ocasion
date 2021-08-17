09b58c925d89897132aa0a87d9e6401b99e73b91
ja54312 <ija54312@gmail.com>
2021-08-16 20:51:27 -0500

    npm instalacion base

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+export default function App(){
+  return(
+    <>
+       <h1>holi crayoli</h1>
+    
+    </>
+  )
+}
+
+const entryPointMalta = document.getElementById("root");
+ReactDOM.render(<App/>, entryPointMalta);
